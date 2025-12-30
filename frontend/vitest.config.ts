8bdfeed2ed6a4ade057f928d4d64f034e59e18d8
anuragdevops2718 <anuragdev2718@gmail.com>
2025-12-30 08:44:30 +0530

    updating biome deps

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	plugins: [react()],
 	test: {
-  reporters: ['default', 'junit'],
-  outputFile: {
-    junit: './test-results/junit.xml'
-        },
+		reporters: ["default", "junit"],
+		outputFile: {
+			junit: "./test-results/junit.xml",
+		},
 		coverage: {
 			provider: "v8", // ✅ fastest
 			reporter: ["text", "html", "lcov", "cobertura"],
